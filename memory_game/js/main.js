83bfa501ecc2e56aa386d5d4953ac1931d298662
Will <will-laws1@Wills-MacBook-Pro.local>
2020-01-14 13:41:38 -0800

    created objects in cards array

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,35 @@
 //define and monitor cards in play
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 const cardsInPlay = [];
 
 //check for match and alert users of result
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  		console.log("You found a match!");
+  		alert("You found a match!");
 	} else {
-  		console.log("Sorry, try again.");
+  		alert("Sorry, try again.");
 }
 }
 
@@ -16,10 +38,13 @@ function flipCard (cardId) {
 	if (cardsInPlay.length === 2) {
 	checkForMatch();
 }
-	console.log ("User flipped " + cards[cardId]);
+	console.log ("User flipped " + cards[cardId].rank);
+	console.log (cards[cardId].suit);
+	console.log (cards[cardId].cardImage);
 //add user's choices to array
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 }
 
 flipCard(0);
-flipCard(3);
+flipCard(1);
+checkForMatch();
